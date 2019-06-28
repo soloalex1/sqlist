7ec4df61ecb7cb71b386d4899c0162f99f05c486
Alexandre Machado <amch.smd@gmail.com>
2019-06-28 10:24:07 -0300

    COMO?

--- app/src/main/java/com/example/sqlist/SearchActivity.java
+++ app/src/main/java/com/example/sqlist/SearchActivity.java
@@ -6,28 +6,54 @@ import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
 import com.example.sqlist.model.ControllerDB;
 import com.example.sqlist.model.CreateDB;
 
-public class SearchActivity extends Activity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchActivity extends Activity implements View.OnClickListener {
 
     private ListView listaTarefas;
+    public Button btnAddTarefa;
+    public List<TextView> titulosTarefa;
+
+//    @Override
+//    public void onStart(){
+//        super.onStart();
+//        ControllerDB crud = new ControllerDB(getBaseContext());
+//        Cursor c = crud.carregarTarefas();
+//
+//        if(c != null) c.moveToFirst();
+//
+//        for(int i = 0; i < listaTarefas.getCount(); i++){
+//            listaTarefas.addView(titulosTarefa.get(i));
+//        }
+//    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list);        
+        setContentView(R.layout.activity_list);
+
+        btnAddTarefa = findViewById(R.id.btnNovaTarefa);
+        btnAddTarefa.setOnClickListener(this);
+
+        titulosTarefa = new ArrayList<>(10);
 
         ControllerDB crud = new ControllerDB(getBaseContext());
         final Cursor cursor = crud.carregarTarefas();
 
-        String[] fieldNames = new String[] { CreateDB.getID(), CreateDB.getTitulo(), CreateDB.getStatus() };
-        int[] idViews = new int[] { R.id.nomeTarefa, R.id.statusTarefa };
+        String[] fieldNames = new String[] { CreateDB.getID(), CreateDB.getTitulo() };
+        int[] idViews = new int[] { android.R.id.text2, android.R.id.text1 };
         
-        SimpleCursorAdapter adapter = new SimpleCursorAdapter(getBaseContext(), R.layout.activity_list, cursor, fieldNames, idViews, 0);
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(getBaseContext(), android.R.layout.activity_list_item, cursor, fieldNames, idViews, 0);
         
         listaTarefas = findViewById(R.id.listaTarefas);
         listaTarefas.setAdapter(adapter);
@@ -44,5 +70,12 @@ public class SearchActivity extends Activity {
                 finish();
             }
         });
+    }
+
+    @Override
+    public void onClick(View v){
+        if(v == btnAddTarefa){
+            startActivity(new Intent(SearchActivity.this, InsertActivity.class));
+        }
     }
 }
